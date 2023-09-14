791182fbf8794b6d051d888dd6e9a8b8103425d7
MiguelPasaje <cartel-dy@hotmail.com>
2023-09-14 01:36:42 -0500

    piano

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'piano';
+
+  aplicarSonido(numero:number):void{
+    const audio = new Audio();
+
+    audio.src = `../assets/sonidos/note${numero}.wav`
+    audio.load();
+    audio.play();
+
+  }
+
+  @HostListener('window:keydown',['$event'])
+  handlekeyboardEvent(event: KeyboardEvent){
+    const numero = parseInt(event.key)
+
+    if (!isNaN(numero) && numero >= 1 && numero <= 7) {
+      this.aplicarSonido(numero)
+
+    }
+  }
 }
